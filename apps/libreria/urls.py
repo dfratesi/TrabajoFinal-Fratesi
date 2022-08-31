8695898a7c274ca3af2006a8648566671aea4bef
Damian Fratesi <dfratesi@users.noreply.github.com>
2022-08-31 15:32:43 -0300

    url libreria

--- apps/libreria/urls.py
+++ apps/libreria/urls.py
@@ -19,6 +19,7 @@ from .views import (
 app_name = "libreria"
 
 urlpatterns = [
+    # URLs libros
     path("", BookListView.as_view(), name="book-list"),
     path("create/", BookCreateView.as_view(), name="create"),
     path("<int:pk>/", BookDetailView.as_view(), name="book-detail"),
@@ -26,11 +27,15 @@ urlpatterns = [
     path("<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
     path("crud/", BookCrudListView.as_view(), name="book-crud"),
     path("search/", search_books, name="search-book"),
-    path("autores/", AuthorListView.as_view(), name="autores-list"),
-    path("autores/create/", autor_create, name="autor-create"),
-    path("autores/<int:pk>/", autor_detail, name="autor-detail"),
+    # URLs autores
+    path("author/", AuthorListView.as_view(), name="autores-list"),
+    path("author/create/", autor_create, name="autor-create"),
+    path("author/<int:pk>/", autor_detail, name="autor-detail"),
+    #path("author/crud/", autor_detail, name="autor-crud"),
+    # URLs generos
     path("genres/", lista_generos, name="genre-list"),
     path("genres/create/", genero_create, name="genre-create"),
     path("genres/<int:pk>/", genero_detail, name="genre-detail"),
+    # URLs idiomas
     path("idiomas/create/", idioma_create, name="idiomas-create"),
 ]
