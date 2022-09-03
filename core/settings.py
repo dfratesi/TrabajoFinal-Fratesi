c0464f9b4d3619af26f3062fb0d5f98600991a40
Damian Fratesi <dfratesi@yahoo.com>
2022-09-03 17:19:02 -0300

    footer y templates

--- core/settings.py
+++ core/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIR = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media/"
