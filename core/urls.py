392e031019d40d2d979bf8347399d44a32947df3
Damian Fratesi <dfratesi@yahoo.com>
2022-09-05 20:24:39 -0300

    urls

--- core/urls.py
+++ core/urls.py
@@ -34,7 +34,6 @@ urlpatterns = (
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
-print(urlpatterns)
 
 if settings.DEBUG:
     import debug_toolbar
